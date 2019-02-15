926ebf79d1712683f52fa5d224d73e907563d382
borisdipp <boris.dipp@gmail.com>
2019-02-15 11:18:22 +0100

    add docker

--- src/main/java/de/tsystems/helloSBS/HelloSBSApplication.java
+++ src/main/java/de/tsystems/helloSBS/HelloSBSApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloSbsApplication {
+public class HelloSBSApplication
+{
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSbsApplication.class, args);
+		SpringApplication.run(HelloSBSApplication.class, args);
 	}
 
 }
